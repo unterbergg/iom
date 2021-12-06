853c0872b81caa5db36539a0b5efae5f94d51dae
vladislav.unterberg <vladislav.unterberg@solbeg.com>
2021-12-06 16:32:15 +0300

    add date_format, timezone and messenger field in get/update endpoint

--- inc/api/api.route.update-user-info.php
+++ inc/api/api.route.update-user-info.php
@@ -14,7 +14,6 @@ add_action( 'rest_api_init', function () {
         'callback' => function ($data) {
 
             $data = $data->get_params();
-
             $user = healthos_get_user( $data['id'] );
 
             if( ! $user ) {
@@ -22,9 +21,18 @@ add_action( 'rest_api_init', function () {
             }
 
             // TODO: update user field(not meta)
-            if ($data['user_email']) {
-                $data['user_login'] = $data['user_email'];
-            }
+
+            $userdata = array(
+                'user_login' => $data['user_email'],
+                'user_email' => $data['user_email'],
+                'first_name' => $data['first_name'],
+                'last_name' => $data['last_name'],
+                'display_name' => $data['display_name'],
+            );
+
+            $userdata['display_name'] = $data['display_name'] ?? $data['first_name'] . " " . $data['last_name'];
+
+            $user = $user->update_user_data($userdata);
 
             $defaults = array(
                 'user_group' => 3830,
@@ -41,7 +49,10 @@ add_action( 'rest_api_init', function () {
                 'ld_group',
                 'user_group',
                 'user_parent',
-                'phone_number'
+                'phone_number',
+                'date_format',
+                'timezone',
+                'messenger'
             );
 
             foreach ( $meta_keys as $key ) {
@@ -90,13 +101,51 @@ add_action( 'rest_api_init', function () {
             'timezone' => array(
                 'default' => null,
             ),
+            // TODO: Review
             'date_format' => array(
                 'default' => array(
-                    'key1' => 'value1',
-                    'key2' => 'value2',
-                    'key3' => 'value3'
+                    'MM/DD/YYYY' => false,
+                    'DD/MM/YYYY' => false,
+                    'YYYY/MM/DD' => false
                 )
-            )
+            ),
+            // TODO: Review
+            'date_format' => array(
+                'default' => array(
+                    '(GMT +00:00) GMT' => false,
+                    '(GMT +00:00) UTC' => false,
+                    '(GMT +01:00) ECT' => false,
+                    '(GMT +02:00) EET' => false,
+                    '(GMT +02:00) ART' => false,
+                    '(GMT +03:00) EAT' => false,
+                    '(GMT +03:30) MET' => false,
+                    '(GMT +04:00) NET' => false,
+                    '(GMT +05:00) PLT' => false,
+                    '(GMT +05:30) IST' => false,
+                    '(GMT +06:00) BST' => false,
+                    '(GMT +07:00) VST' => false,
+                    '(GMT +08:00) CTT' => false,
+                    '(GMT +09:00) JST' => false,
+                    '(GMT +09:30) ACT' => false,
+                    '(GMT +10:00) AET' => false,
+                    '(GMT +11:00) SST' => false,
+                    '(GMT +12:00) NST' => false,
+                    '(GMT -11:00) MIT' => false,
+                    '(GMT -10:00) HST' => false,
+                    '(GMT -09:00) AST' => false,
+                    '(GMT -08:00) PST' => false,
+                    '(GMT -07:00) PNT' => false,
+                    '(GMT -07:00) MST' => false,
+                    '(GMT -06:00) CST' => false,
+                    '(GMT -05:00) EST' => false,
+                    '(GMT -05:00) IET' => false,
+                    '(GMT -04:00) PRT' => false,
+                    '(GMT -03:30) CNT' => false,
+                    '(GMT -03:00) AGT' => false,
+                    '(GMT -03:00) BET' => false,
+                    '(GMT -01:00) CAT' => false,
+                )
+            ),
         )
 
     ));
--- inc/class/class.user.php
+++ inc/class/class.user.php
@@ -18,6 +18,55 @@ class HOS_User extends WP_User
      *
      **/
 
+    private $dateformat_list = [
+        'MM/DD/YYYY',
+        'DD/MM/YYYY',
+        'YYYY/MM/DD'
+    ];
+    private $timezone_list = [
+        '(GMT +00:00) GMT',
+        '(GMT +00:00) UTC',
+        '(GMT +01:00) ECT',
+        '(GMT +02:00) EET',
+        '(GMT +02:00) ART',
+        '(GMT +03:00) EAT',
+        '(GMT +03:30) MET',
+        '(GMT +04:00) NET',
+        '(GMT +05:00) PLT',
+        '(GMT +05:30) IST',
+        '(GMT +06:00) BST',
+        '(GMT +07:00) VST',
+        '(GMT +08:00) CTT',
+        '(GMT +09:00) JST',
+        '(GMT +09:30) ACT',
+        '(GMT +10:00) AET',
+        '(GMT +11:00) SST',
+        '(GMT +12:00) NST',
+        '(GMT -11:00) MIT',
+        '(GMT -10:00) HST',
+        '(GMT -09:00) AST',
+        '(GMT -08:00) PST',
+        '(GMT -07:00) PNT',
+        '(GMT -07:00) MST',
+        '(GMT -06:00) CST',
+        '(GMT -05:00) EST',
+        '(GMT -05:00) IET',
+        '(GMT -04:00) PRT',
+        '(GMT -03:30) CNT',
+        '(GMT -03:00) AGT',
+        '(GMT -03:00) BET',
+        '(GMT -01:00) CAT',
+    ];
+    private $messenger_list = [
+        'Facebook' => false,
+        'Skype' => false,
+        'WhatsApp' => false,
+        'WeChat' => false,
+        'Twitter' => false,
+        'Telegram' => false,
+        'Signal' => false
+    ];
+
     public function __construct($user_id = false)
     {
 
@@ -53,7 +102,7 @@ class HOS_User extends WP_User
         $userdata = $this->to_array();
         unset( $userdata['user_pass']);
         unset( $userdata['user_activation_key']);
-        $userdata = array_merge($userdata, $this->get_user_meta_callback());
+        $userdata = array_merge($userdata, $this->get_user_meta());
         return $userdata;
     }
 
@@ -62,15 +111,36 @@ class HOS_User extends WP_User
         return ['user_pass' => $userdata['user_pass'], 'ID' => $this->ID];
     }
 
-    private function get_user_meta_callback( ) {
-        return get_user_meta( $this->ID );
+    private function get_user_meta( ) {
+        $usermeta = get_user_meta( $this->ID );
+        $usermeta['date_format'] = $this->get_formated_field($usermeta['date_format'][0], $this->dateformat_list);
+        $usermeta['timezonez'] = $this->get_formated_field($usermeta['timezone'][0], $this->timezone_list);
+        $usermeta['messenger'] = $this->get_multiple_field($usermeta['messenger'][0], $this->messenger_list);
+        return $usermeta;
     }
 
-    public function update_user_data() {
-        $userdata = $this->to_array();
-        unset( $userdata['user_pass']);
-        unset( $userdata['user_activation_key']);
-        $userdata = array_merge($userdata, $this->get_user_meta_callback());
-        return $userdata;
+    public function update_user_data( $userdata ) {
+        $userdata['ID']  = $this->ID;
+        $user_id = wp_update_user( $userdata );
+        return healthos_get_user($user_id);
+    }
+
+    public function get_formated_field($usermeta, $field) {
+        $result = [];
+        foreach ($field as $value) {
+            $result[$value] = $value == $usermeta;
+        }
+        return $result;
+    }
+
+    public function get_multiple_field($usermeta, $field) {
+        $result = $field;
+        $usermeta = json_decode($usermeta, true);
+        foreach ($field as $key => $value) {
+            if (array_key_exists( strtolower($key), $usermeta)) {
+                $result[$key] = $usermeta[strtolower($key)];
+            }
+        }
+        return $result;
     }
 }
